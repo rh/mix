d4cacc1f720b7d093cb5088814ce75bc07283e22
Richard Hubers <richard.hubers@gmail.com>
2007-02-05 19:53:18 +0000

    Removed target 'clean' from default.build; it is no longer necessary. Removed target 'documentation' from default.build.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 [assembly: AssemblyConfigurationAttribute("Debug")]
 [assembly: AssemblyVersionAttribute("0.1.0.0")]
 [assembly: AssemblyFileVersionAttribute("0.1.0.0")]
-[assembly: AssemblyCopyrightAttribute("Copyright © 2007 Richard Hubers")]
+[assembly: AssemblyCopyrightAttribute("Copyright © 2006-2007 Richard Hubers")]
 [assembly: AssemblyCompanyAttribute("mix.sourceforge.net")]
 [assembly: AssemblyProductAttribute("Mix")]
