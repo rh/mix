2f9aded30873021b1708c0666864472bc2fe75a6
Richard Hubers <richard.hubers@gmail.com>
2007-02-27 17:43:58 +0000

    Added CopyAction and CopyActionFixture to Mix.Actions.csproj and Mix.Actions.Tests.csproj and registered CopyAction.

--- src/Mix.Console/Commands/CommandFactory.cs
+++ src/Mix.Console/Commands/CommandFactory.cs
@@ -29,6 +29,7 @@ namespace Mix.Console.Commands
             Register(new ActionCommand(new AppendAction()));
             Register(new ActionCommand(new UpperCaseAction()));
             Register(new ActionCommand(new ReplaceAction()));
+            Register(new ActionCommand(new CopyAction()));
             Register(new ActionCommand(new CopyAttributeAction()));
             Register(new ActionCommand(new InnerXmlAction()));
         }
--- src/Mix.Console/Commands/ListCommand.cs
+++ src/Mix.Console/Commands/ListCommand.cs
@@ -1,76 +1,81 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Reflection;
 using Mix.Core;
 using Mix.Core.Attributes;
 
 namespace Mix.Console.Commands
 {
-    [Description("Shows a list of all available actions.")]
-    internal sealed class ListCommand : Command
-    {
-        public override int Execute()
-        {
-            WriteLine("Available actions:");
-            foreach (Type type in Actions())
-            {
-                WriteLine("  {0}", type.Name.Replace("Action", "").ToLower());
-            }
-            WriteLine("\nType 'mix help <action>' for help on a specific action.");
-            return 0;
-        }
+	[Description("Shows a list of all available actions.")]
+	internal sealed class ListCommand : Command
+	{
+		public override int Execute()
+		{
+			WriteLine("Available actions:");
+			foreach (Type type in Actions())
+			{
+				WriteLine("  {0}", type.Name.Replace("Action", "").ToLower());
+			}
+			WriteLine("\nType 'mix help <action>' for help on a specific action.");
+			return 0;
+		}
 
-        private static IList<Type> types;
+		private static IList<Type> types;
 
-        public static IList<Type> Actions()
-        {
-            if (types == null)
-            {
-                types = new List<Type>();
-                // Explicitly load assembly Mix.Actions
-                Assembly.Load("Mix.Actions");
-                Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
-                AddActions(assemblies);
-            }
-            return types;
-        }
+		public static IList<Type> Actions()
+		{
+			if (types == null)
+			{
+				types = new List<Type>();
+				// Explicitly load assembly Mix.Actions
+				Assembly.Load("Mix.Actions");
+				Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+				AddActions(assemblies);
+			}
+			return types;
+		}
 
-        private static void AddActions(Assembly[] assemblies)
-        {
-            Check.ArgumentIsNotNull(assemblies, "assemblies");
+		private static void AddActions(Assembly[] assemblies)
+		{
+			Check.ArgumentIsNotNull(assemblies, "assemblies");
 
-            foreach (Assembly assembly in assemblies)
-            {
-                if (IsSystemAssembly(assembly))
-                {
-                    continue;
-                }
+			foreach (Assembly assembly in assemblies)
+			{
+				if (IsSystemAssembly(assembly))
+				{
+					continue;
+				}
 
-                foreach (Type type in assembly.GetTypes())
-                {
-                    if (IsAction(type))
-                    {
-                        if (!types.Contains(type))
-                        {
-                            types.Add(type);
-                        }
-                    }
-                }
-            }
-        }
+				foreach (Type type in assembly.GetTypes())
+				{
+					if (IsAction(type))
+					{
+						if (!types.Contains(type))
+						{
+							types.Add(type);
+						}
+					}
+				}
+			}
+		}
 
-        private static bool IsSystemAssembly(Assembly assembly)
-        {
-            AssemblyName name = assembly.GetName();
-            return name.Name.ToLower() == "mscorlib" ||
-                   name.Name.ToLower().StartsWith("system");
-        }
+		[DebuggerStepThrough]
+		private static bool IsSystemAssembly(Assembly assembly)
+		{
+			string name = assembly.GetName().ToString().ToLower();
+			return name.StartsWith("system") ||
+			       name.StartsWith("microsoft") ||
+			       name.StartsWith("vshost") ||
+			       name == "mscorlib";
+		}
 
-        private static bool IsAction(Type type)
-        {
-            return typeof(Action).IsAssignableFrom(type) &&
-                   !type.IsInterface &&
-                   !type.IsAbstract;
-        }
-    }
+		[DebuggerStepThrough]
+		private static bool IsAction(Type type)
+		{
+			return typeof(Action).IsAssignableFrom(type) &&
+			       !type.IsInterface &&
+			       !type.IsAbstract;
+		}
+	}
 }
