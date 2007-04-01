8b4a844b389a501378d09ec192e11ea615e4fadd
Richard Hubers <richard.hubers@gmail.com>
2007-04-01 19:47:45 +0000

    Refactored ListCommand; all reflection code was moved to ActionInfo. ListCommand now only uses IActionInfo.

--- src/Mix.Console/Commands/ListCommand.cs
+++ src/Mix.Console/Commands/ListCommand.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
 using Mix.Core;
 using Mix.Core.Attributes;
 
@@ -13,69 +10,13 @@ namespace Mix.Console.Commands
         public override int Execute()
         {
             WriteLine("Available actions:");
-            foreach (Type type in Actions())
+            foreach (IActionInfo info in ActionInfo.All())
             {
-                WriteLine("  {0}", type.Name.Replace("Action", "").ToLower());
+                WriteLine("  {0}", info.Name);
             }
-            WriteLine("\nType 'mix help <action>' for help on a specific action.");
+            Write(Environment.NewLine);
+            WriteLine("Type 'mix help <action>' for help on a specific action.");
             return 0;
-        }
-
-        private static IList<Type> types;
-
-        public static IList<Type> Actions()
-        {
-            if (types == null)
-            {
-                types = new List<Type>();
-                // Explicitly load assembly Mix.Actions
-                Assembly.Load("Mix.Actions");
-                Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
-                AddActions(assemblies);
-            }
-            return types;
-        }
-
-        private static void AddActions(Assembly[] assemblies)
-        {
-            Check.ArgumentIsNotNull(assemblies, "assemblies");
-
-            foreach (Assembly assembly in assemblies)
-            {
-                if (IsSystemAssembly(assembly))
-                {
-                    continue;
-                }
-
-                foreach (Type type in assembly.GetTypes())
-                {
-                    if (IsAction(type))
-                    {
-                        if (!types.Contains(type))
-                        {
-                            types.Add(type);
-                        }
-                    }
-                }
-            }
-        }
-
-        [DebuggerStepThrough]
-        private static bool IsSystemAssembly(Assembly assembly)
-        {
-            string name = assembly.GetName().ToString().ToLower();
-            return name.StartsWith("system") ||
-                   name.StartsWith("microsoft") ||
-                   name.StartsWith("vshost") ||
-                   name == "mscorlib";
-        }
-
-        [DebuggerStepThrough]
-        private static bool IsAction(Type type)
-        {
-            return typeof(Action).IsAssignableFrom(type) &&
-                   !type.IsInterface &&
-                   !type.IsAbstract;
         }
     }
 }
--- src/Mix.Core/ActionInfo.cs
+++ src/Mix.Core/ActionInfo.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
 using Mix.Core.Attributes;
 
 namespace Mix.Core
@@ -43,5 +46,78 @@ namespace Mix.Core
             info.arguments = ArgumentInfo.For(obj);
             return info;
         }
+
+        public static IActionInfo[] All()
+        {
+            IList<Type> types = Actions();
+            IActionInfo[] infos = new IActionInfo[types.Count];
+            for (int i = 0; i < types.Count; i++)
+            {
+                object obj = Activator.CreateInstance(types[i]);
+                infos[i] = For(obj);
+            }
+            return infos;
+        }
+
+        #region Reflection Helpers
+
+        private static IList<Type> actionTypes;
+
+        private static IList<Type> Actions()
+        {
+            if (actionTypes == null)
+            {
+                actionTypes = new List<Type>();
+                // Explicitly load assembly Mix.Actions
+                Assembly.Load("Mix.Actions");
+                Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+                AddActions(assemblies);
+            }
+            return actionTypes;
+        }
+
+        private static void AddActions(Assembly[] assemblies)
+        {
+            Check.ArgumentIsNotNull(assemblies, "assemblies");
+
+            foreach (Assembly assembly in assemblies)
+            {
+                if (IsSystemAssembly(assembly))
+                {
+                    continue;
+                }
+
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (IsAction(type))
+                    {
+                        if (!actionTypes.Contains(type))
+                        {
+                            actionTypes.Add(type);
+                        }
+                    }
+                }
+            }
+        }
+
+        [DebuggerStepThrough]
+        private static bool IsSystemAssembly(Assembly assembly)
+        {
+            string name = assembly.GetName().ToString().ToLower();
+            return name.StartsWith("system") ||
+                   name.StartsWith("microsoft") ||
+                   name.StartsWith("vshost") ||
+                   name == "mscorlib";
+        }
+
+        [DebuggerStepThrough]
+        private static bool IsAction(Type type)
+        {
+            return typeof(Action).IsAssignableFrom(type) &&
+                   !type.IsInterface &&
+                   !type.IsAbstract;
+        }
+
+        #endregion
     }
 }
