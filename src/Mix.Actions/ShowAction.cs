627643bce55c86188d8fe53a18daeb99926ceb56
Richard Hubers <richard.hubers@gmail.com>
2008-03-22 16:26:58 +0000

    Reverted r111 for ShowAction.

--- src/Mix.Actions/ShowAction.cs
+++ src/Mix.Actions/ShowAction.cs
@@ -1,23 +1,44 @@
 using System;
-using System.Xml;
+using System.IO;
+using System.Xml.XPath;
 using Mix.Core;
 using Mix.Core.Attributes;
+using Mix.Core.Exceptions;
 
 namespace Mix.Actions
 {
     [Description("Shows all selected nodes.")]
     public class ShowAction : Action, IReadOnly
     {
-        protected override void OnBeforeExecute(int count)
+        protected override bool ExecuteCore(IContext context)
         {
-            Context.Output.WriteLine("{0}: {1}", Context.FileName, count);
-        }
-
-        protected override void ExecuteCore(XmlNode node)
-        {
-            string xml = node.OuterXml.Trim();
-            xml = xml.Replace(Environment.NewLine, String.Format("{0}  ", Environment.NewLine));
-            Context.Output.WriteLine("  {0}", xml);
+            if (!String.IsNullOrEmpty(context.XPath))
+            {
+                using (TextReader reader = new StringReader(context.Xml))
+                {
+                    XPathDocument document = new XPathDocument(reader);
+                    XPathNavigator navigator = document.CreateNavigator();
+                    try
+                    {
+                        XPathNodeIterator iterator = navigator.Select(context.XPath);
+                        context.Output.WriteLine("{0}: {1}", context.FileName, iterator.Count);
+                        while (iterator.MoveNext())
+                        {
+                            context.Output.WriteLine(iterator.Current.OuterXml.Trim());
+                        }
+                    }
+                    catch (XPathException e)
+                    {
+                        string message = String.Format("'{0}' is not a valid XPath expression: {1}{2}", context.XPath, Environment.NewLine, e.Message);
+                        throw new ActionExecutionException(message, e);
+                    }
+                }
+            }
+            else
+            {
+                context.Output.WriteLine("{0}: no selection.", context.FileName);
+            }
+            return true;
         }
     }
 }
