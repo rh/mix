07482a98498b8e3a0fec1f8918d8031db4f1cc70
Richard Hubers <richard.hubers@gmail.com>
2008-03-22 14:25:18 +0000

    Changed AddCDataSectionAction: because CDATA sections can only be added to elements, ExecuteCore(XmlAttribute) was removed. Two tests needed to be removed.

--- src/Mix.Actions.Tests/AddCDataSectionActionFixture.cs
+++ src/Mix.Actions.Tests/AddCDataSectionActionFixture.cs
@@ -17,28 +17,6 @@ namespace Mix.Actions.Tests
         }
 
         [Test]
-        public void AddToAttribute()
-        {
-            const string pre = @"<root a=""""></root>";
-            const string post = @"<root a=""""><![CDATA[text]]></root>";
-            const string xpath = "root/@a";
-            AddCDataSectionAction action = new AddCDataSectionAction();
-            action.Value = "text";
-            Run(pre, post, xpath, action);
-        }
-
-        [Test]
-        public void AddToTextNode()
-        {
-            const string pre = @"<root>text</root>";
-            const string post = @"<root>text<![CDATA[text]]></root>";
-            const string xpath = "//text()";
-            AddCDataSectionAction action = new AddCDataSectionAction();
-            action.Value = "text";
-            Run(pre, post, xpath, action);
-        }
-
-        [Test]
         public void AddToCDataSection()
         {
             const string pre = @"<root><![CDATA[text]]></root>";
--- src/Mix.Actions/AddCDataSectionAction.cs
+++ src/Mix.Actions/AddCDataSectionAction.cs
@@ -5,11 +5,19 @@ using Mix.Core.Attributes;
 
 namespace Mix.Actions
 {
+    /// <summary>
+    /// Adds an <see cref="XmlCDataSection"/> to the selected <see cref="XmlElement"/>(s).
+    /// If or adds the value of the new <see cref="XmlCDataSection"/> to 
+    /// </summary>
     [Description("Adds a new CDATA section.")]
     public class AddCDataSectionAction : Action
     {
         private string @value = String.Empty;
 
+        /// <summary>
+        /// Gets or sets the value of the <see cref="XmlCDataSection"/>.
+        /// This argument is not required. If it is not set, an empty <see cref="XmlCDataSection"/> will be added.
+        /// </summary>
         [Argument]
         [Description("The value of the CDATA section.")]
         public virtual string Value
@@ -18,22 +26,24 @@ namespace Mix.Actions
             set { this.@value = value; }
         }
 
+        /// <summary>
+        /// Adds an <see cref="XmlCDataSection"/> to <paramref name="element"/>.
+        /// </summary>
+        /// <param name="element">
+        /// The <see cref="XmlElement"/> to which an <see cref="XmlCDataSection"/> should be added.
+        /// </param>
         protected override void ExecuteCore(XmlElement element)
         {
             XmlCDataSection section = element.OwnerDocument.CreateCDataSection(Value);
             element.AppendChild(section);
         }
 
-        protected override void ExecuteCore(XmlAttribute attribute)
-        {
-            ExecuteCore(attribute.OwnerElement);
-        }
-
-        protected override void ExecuteCore(XmlText text)
-        {
-            ExecuteCore(text.ParentNode as XmlElement);
-        }
-
+        /// <summary>
+        /// Adds the value of the new <see cref="XmlCDataSection"/> (e.g. <see cref="Value"/>) to the value of <paramref name="section"/>.
+        /// </summary>
+        /// <param name="section">
+        /// The <see cref="XmlCDataSection"/> to which <see cref="Value"/> should be added.
+        /// </param>
         protected override void ExecuteCore(XmlCDataSection section)
         {
             section.Value = section.Value + Value;
