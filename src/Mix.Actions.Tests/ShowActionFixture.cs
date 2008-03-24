60b3d8db821562a3192bdccf6f3d823112b2a089
Richard Hubers <richard.hubers@gmail.com>
2008-03-24 09:51:57 +0000

    Added some tests for ShowAction.

---
+++ src/Mix.Actions.Tests/ShowActionFixture.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using Mix.Core;
+using Mix.Core.Exceptions;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace Mix.Actions.Tests
+{
+    [TestFixture]
+    public class ShowActionFixture : TestFixture
+    {
+        [Test]
+        public void Count0()
+        {
+            using (TextWriter writer = new StringWriter())
+            {
+                Context context = new Context("<root />", "foo", writer);
+                context.FileName = "file";
+                ShowAction action = new ShowAction();
+                action.Execute(context);
+                Assert.That(writer.ToString(), Is.EqualTo(String.Format("file: 0{0}", Environment.NewLine)));
+            }
+        }
+
+        [Test]
+        public void NoSelection()
+        {
+            using (TextWriter writer = new StringWriter())
+            {
+                Context context = new Context();
+                context.Xml = "<root/>";
+                context.FileName = "file";
+                context.Output = writer;
+                ShowAction action = new ShowAction();
+                action.Execute(context);
+                Assert.That(writer.ToString(), Is.EqualTo(String.Format("file: no selection.{0}", Environment.NewLine)));
+            }
+        }
+
+        [Test]
+        [ExpectedException(typeof(ActionExecutionException))]
+        public void InvalidXPathExpression()
+        {
+            using (TextWriter writer = new StringWriter())
+            {
+                Context context = new Context("<root/>", "///");
+                context.FileName = "file";
+                context.Output = writer;
+                ShowAction action = new ShowAction();
+                action.Execute(context);
+            }
+        }
+    }
+}
