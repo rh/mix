94a8605a3296dab7913aed3afcc060c1a473636c
Richard Hubers <richard.hubers@gmail.com>
2008-11-01 10:45:42 +0000

    Added the possibility to show line numbers, but they are currently turned off.

--- src/Mix.Actions/Show.cs
+++ src/Mix.Actions/Show.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Xml;
 using System.Xml.XPath;
 using Mix.Core;
 using Mix.Core.Attributes;
@@ -10,6 +11,14 @@ namespace Mix.Actions
     [Description("Shows all selected nodes.")]
     public class Show : Action, IReadOnly
     {
+        private bool showLineNumbers;
+
+        public bool ShowLineNumbers
+        {
+            get { return showLineNumbers; }
+            set { showLineNumbers = value; }
+        }
+
         protected override bool ExecuteCore(IContext context)
         {
             if (!String.IsNullOrEmpty(context.XPath))
@@ -24,7 +33,18 @@ namespace Mix.Actions
                         context.Output.WriteLine("{0}: {1}", context.FileName, iterator.Count);
                         while (iterator.MoveNext())
                         {
-                            context.Output.WriteLine(iterator.Current.OuterXml.Trim());
+                            XPathNavigator current = iterator.Current;
+                            IXmlLineInfo info = current as IXmlLineInfo;
+                            if (info != null)
+                            {
+                                string prefix = showLineNumbers ? String.Format("{0,4}: ", info.LineNumber) : String.Empty;
+                                string xml = prefix + current.OuterXml.Trim().Replace(Environment.NewLine, Environment.NewLine + new string(' ', prefix.Length));
+                                context.Output.WriteLine(xml);
+                            }
+                            else
+                            {
+                                context.Output.WriteLine(current.OuterXml.Trim());
+                            }
                         }
                     }
                     catch (XPathException e)
