af0c7743124bf0275926400ec28fafbf3ea8cdc7
Richard Hubers <richard.hubers@gmail.com>
2009-01-28 17:06:38 +0000

    Initial implementation of Move (currently only implemented for elements).

---
+++ src/Mix.Actions/Move.cs
@@ -0,0 +1,29 @@
+using System.Xml;
+using Mix.Core;
+using Mix.Core.Attributes;
+
+namespace Mix.Actions
+{
+    public class Move : Action
+    {
+        [Argument, Required]
+        [Description("An XPath expression, relative to the selected node, which determines to which node the selected node should be moved.")]
+        public string To { get; set; }
+
+        protected override void ExecuteCore(XmlElement element)
+        {
+            // TODO: validate XPath expression
+            var newParent = element.SelectSingleNode(To) as XmlElement;
+            if (newParent != null)
+            {
+                var clone = element.CloneNode(true);
+                newParent.AppendChild(clone);
+                element.ParentNode.RemoveChild(element);
+            }
+            else
+            {
+                Context.Output.WriteLine("");
+            }
+        }
+    }
+}
