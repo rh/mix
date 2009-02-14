49d5567d2260bb78fe06ba61157f3568b03e18aa
Richard Hubers <richard.hubers@gmail.com>
2009-02-14 18:50:49 +0000

    Added task convert-to-text.

---
+++ src/Mix.Tasks.Tests/ConvertToTextFixture.cs
@@ -0,0 +1,78 @@
+using NUnit.Framework;
+
+namespace Mix.Tasks.Tests
+{
+    [TestFixture]
+    public class ConvertToTextFixture : TestFixture
+    {
+        [Test]
+        public void ConvertElementToText()
+        {
+            const string pre = @"<root><foo>bar</foo></root>";
+            const string post = @"<root>&lt;foo&gt;bar&lt;/foo&gt;</root>";
+            const string xpath = "//foo";
+            var task = new ConvertToText();
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void ConvertCDataSectionToText()
+        {
+            const string pre = @"<root><![CDATA[text]]></root>";
+            const string post = @"<root>text</root>";
+            const string xpath = "//text()";
+            var task = new ConvertToText();
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void ConvertCDataSectionWithSpecialCharactersToText()
+        {
+            const string pre = @"<root><![CDATA[<text>]]></root>";
+            const string post = @"<root>&lt;text&gt;</root>";
+            const string xpath = "//text()";
+            var task = new ConvertToText();
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void ConvertCommentToText()
+        {
+            const string pre = @"<root><!--text--></root>";
+            const string post = @"<root>text</root>";
+            const string xpath = "//comment()";
+            var task = new ConvertToText();
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void ConvertCommentWithSpecialCharactersToText()
+        {
+            const string pre = @"<root><!--<text>--></root>";
+            const string post = @"<root>&lt;text&gt;</root>";
+            const string xpath = "//comment()";
+            var task = new ConvertToText();
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void ConvertProcessingInstructionToText()
+        {
+            const string pre = @"<root><?pi text?></root>";
+            const string post = @"<root>text</root>";
+            const string xpath = "//processing-instruction()";
+            var task = new ConvertToText();
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void ConvertProcessingInstructionWithSpecialCharactersToText()
+        {
+            const string pre = @"<root><?pi <text>?></root>";
+            const string post = @"<root>&lt;text&gt;</root>";
+            const string xpath = "//processing-instruction()";
+            var task = new ConvertToText();
+            Run(pre, post, xpath, task);
+        }
+    }
+}
