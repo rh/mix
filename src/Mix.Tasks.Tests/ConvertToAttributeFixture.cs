f851cad1c6a6387548f433c2a2c2868f85c28d3a
Richard Hubers <richard.hubers@gmail.com>
2009-04-11 19:23:47 +0200

    Improved convert-to-attribute: conversions from text, CDATA, comments and processing instructions are supported.

--- src/Mix.Tasks.Tests/ConvertToAttributeFixture.cs
+++ src/Mix.Tasks.Tests/ConvertToAttributeFixture.cs
@@ -1,3 +1,4 @@
+using Mix.Core.Exceptions;
 using NUnit.Framework;
 
 namespace Mix.Tasks.Tests
@@ -21,6 +22,113 @@ namespace Mix.Tasks.Tests
             const string pre = @"<root><foo><bar />bar</foo></root>";
             const string post = pre;
             const string xpath = "//foo";
+            var task = new ConvertToAttribute();
+            Run(pre, post, xpath, task);
+        }
+
+        [Test, ExpectedException(typeof(RequirementException))]
+        public void NameShouldBeSuppliedForText()
+        {
+            const string pre = @"<root>foo</root>";
+            const string xpath = "//text()";
+            var task = new ConvertToAttribute();
+            Run(pre, null, xpath, task);
+        }
+
+        [Test, ExpectedException(typeof(RequirementException))]
+        public void NameShouldBeSuppliedForCData()
+        {
+            const string pre = @"<root><![CDATA[text]]></root>";
+            const string xpath = "//text()";
+            var task = new ConvertToAttribute();
+            Run(pre, null, xpath, task);
+        }
+
+        [Test, ExpectedException(typeof(RequirementException))]
+        public void NameShouldBeSuppliedForComments()
+        {
+            const string pre = @"<root><!----></root>";
+            const string xpath = "//comment()";
+            var task = new ConvertToAttribute();
+            Run(pre, null, xpath, task);
+        }
+
+        [Test]
+        public void Text()
+        {
+            const string pre = @"<root>text</root>";
+            const string post = @"<root foo=""text""></root>";
+            const string xpath = "//text()";
+            var task = new ConvertToAttribute {Name = "foo"};
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void TextShouldNotBeConvertedWhenTheNamedAttributeAlreadyExists()
+        {
+            const string pre = @"<root foo="""">text</root>";
+            const string post = pre;
+            const string xpath = "//text()";
+            var task = new ConvertToAttribute {Name = "foo"};
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void CData()
+        {
+            const string pre = @"<root><![CDATA[text]]></root>";
+            const string post = @"<root foo=""text""></root>";
+            const string xpath = "//text()";
+            var task = new ConvertToAttribute {Name = "foo"};
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void CDataShouldNotBeConvertedWhenTheNamedAttributeAlreadyExists()
+        {
+            const string pre = @"<root foo=""""><![CDATA[text]]></root>";
+            const string post = pre;
+            const string xpath = "//text()";
+            var task = new ConvertToAttribute {Name = "foo"};
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void Comment()
+        {
+            const string pre = @"<root><!--text--></root>";
+            const string post = @"<root foo=""text""></root>";
+            const string xpath = "//comment()";
+            var task = new ConvertToAttribute {Name = "foo"};
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void CommentShouldNotBeConvertedWhenTheNamedAttributeAlreadyExists()
+        {
+            const string pre = @"<root foo=""""><!--text--></root>";
+            const string post = pre;
+            const string xpath = "//comment()";
+            var task = new ConvertToAttribute {Name = "foo"};
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void ProcessingInstruction()
+        {
+            const string pre = @"<root><?pi text?></root>";
+            const string post = @"<root pi=""text""></root>";
+            const string xpath = "//processing-instruction()";
+            var task = new ConvertToAttribute();
+            Run(pre, post, xpath, task);
+        }
+
+        [Test]
+        public void ProcessingInstructionShouldNotBeConvertedWhenTheNamedAttributeAlreadyExists()
+        {
+            const string pre = @"<root pi=""""><?pi text?></root>";
+            const string post = pre;
+            const string xpath = "//processing-instruction()";
             var task = new ConvertToAttribute();
             Run(pre, post, xpath, task);
         }
--- src/Mix.Tasks/ConvertToAttribute.cs
+++ src/Mix.Tasks/ConvertToAttribute.cs
@@ -1,20 +1,17 @@
 using System.Xml;
 using Mix.Core;
 using Mix.Core.Attributes;
+using Mix.Core.Exceptions;
 
 namespace Mix.Tasks
 {
-    /// <summary>
-    /// This task creates a new attribute with the name and value of the
-    /// selected element. The new attribute is inserted as the last attribute
-    /// of the element's owner element. The element itself is removed.
-    /// If the element is not a text node nothing changes.
-    /// If the element's owner element already has an attribute with the name of
-    /// the element nothing changes.
-    /// </summary>
-    [Description("Converts elements to attributes.")]
+    [Description("Converts the selected nodes to attributes.")]
     public class ConvertToAttribute : Task
     {
+        [Argument]
+        [Description("The name of the attribute. Required for nodes other then elements or processing instructions.")]
+        public string Name { get; set; }
+
         protected override void ExecuteCore(XmlElement element)
         {
             var owner = element.ParentNode as XmlElement;
@@ -30,6 +27,74 @@ namespace Mix.Tasks
                     owner.Attributes.Append(attribute);
                     owner.RemoveChild(element);
                 }
+            }
+        }
+
+        protected override void ExecuteCore(XmlText text)
+        {
+            Validate();
+
+            var element = text.ParentNode as XmlElement;
+
+            if (element != null && element.Attributes[Name] == null)
+            {
+                var attribute = text.OwnerDocument.CreateAttribute(Name);
+                attribute.Value = text.Value;
+                element.Attributes.Append(attribute);
+                element.RemoveChild(text);
+            }
+        }
+
+        protected override void ExecuteCore(XmlCDataSection section)
+        {
+            Validate();
+
+            var element = section.ParentNode as XmlElement;
+
+            if (element != null && element.Attributes[Name] == null)
+            {
+                var attribute = section.OwnerDocument.CreateAttribute(Name);
+                attribute.Value = section.Value;
+                element.Attributes.Append(attribute);
+                element.RemoveChild(section);
+            }
+        }
+
+        protected override void ExecuteCore(XmlComment comment)
+        {
+            Validate();
+
+            var element = comment.ParentNode as XmlElement;
+
+            if (element != null && element.Attributes[Name] == null)
+            {
+                var attribute = comment.OwnerDocument.CreateAttribute(Name);
+                attribute.Value = comment.Value;
+                element.Attributes.Append(attribute);
+                element.RemoveChild(comment);
+            }
+        }
+
+        protected override void ExecuteCore(XmlProcessingInstruction instruction)
+        {
+            var element = instruction.ParentNode as XmlElement;
+            var name = instruction.Name;
+
+            if (element != null && element.Attributes[name] == null)
+            {
+                var attribute = instruction.OwnerDocument.CreateAttribute(name);
+                attribute.Value = instruction.Value;
+                element.Attributes.Append(attribute);
+                element.RemoveChild(instruction);
+            }
+        }
+
+        private void Validate()
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                // TODO: remove duplication
+                throw new RequirementException("'Name' is required when a node other then an element or a processing instruction is selected.", "Name", "The name of the processing instruction. Required for nodes other then elements or processing instructions.");
             }
         }
     }
