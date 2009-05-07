3bee917e43c24afb34fa871992aab3cac2143090
Richard Hubers <richard.hubers@gmail.com>
2009-05-07 17:47:17 +0200

    Added new task unwrap. This task is currently only implemented for elements.

---
+++ src/Mix.Tasks/Unwrap.cs
@@ -0,0 +1,20 @@
+using System.Xml;
+using Mix.Core;
+using Mix.Core.Attributes;
+
+namespace Mix.Tasks
+{
+    [Description("Unwraps the selected nodes, e.g. '<element>text</element>' becomes 'text' and '<!-- text -->' becomes ' text '.")]
+    public class Unwrap : Task
+    {
+        protected override void ExecuteCore(XmlElement element)
+        {
+            foreach(XmlNode node in element.ChildNodes)
+            {
+                var clone = node.CloneNode(true);
+                element.ParentNode.InsertBefore(clone, element);
+            }
+            element.ParentNode.RemoveChild(element);
+        }
+    }
+}
