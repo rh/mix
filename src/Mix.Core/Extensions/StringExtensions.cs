rh/mix

commit b78483659f4ca57a2b7f36fa625f260161549ad9

Fixed a potential bug in Dasherize().

src/Mix.Core/Extensions/StringExtensions.cs
@@ -7,6 +7,11 @@ namespace Mix.Core.Extensions
     {
         public static string Dasherize(this string value)
         {
+            if (string.IsNullOrEmpty(value))
+            {
+                return string;
+            }
+
             var sb = new StringBuilder();
             sb.Append(Char.ToLower(value[0]));
             for (var i = 1; i < value.Length; i++)

commit 85d505031d350b0c59a98e4b68e54cb7a49721bc

Fixed a bug introduced in b784836.

src/Mix.Core/Extensions/StringExtensions.cs
@@ -9,7 +9,7 @@ namespace Mix.Core.Extensions
         {
             if (string.IsNullOrEmpty(value))
             {
-                return string;
+                return value;
             }
 
             var sb = new StringBuilder();