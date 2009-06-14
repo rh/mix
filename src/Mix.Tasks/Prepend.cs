cb437f24fedaef483875fc1a77cd786f4ac1f613
Richard Hubers <richard.hubers@gmail.com>
2009-06-14 21:51:55 +0200

    Converted prepend into a TextTransformer.

--- src/Mix.Tasks/Prepend.cs
+++ src/Mix.Tasks/Prepend.cs
@@ -1,62 +1,17 @@
-using System.Xml;
-using Mix.Core;
 using Mix.Core.Attributes;
 
 namespace Mix.Tasks
 {
     [Description("Prepends text to selected nodes (if they 'behave' like text), or to the value of the selected attributes.")]
-    public class Prepend : Task
+    public class Prepend : TextTransformer
     {
         [Option, Required]
         [Description("The value to prepend.")]
         public string Value { get; set; }
 
-        protected override void ExecuteCore(XmlElement element)
+        protected override string TransformCore(string value)
         {
-            if (element.HasChildNodes)
-            {
-                if (element.ChildNodes.Count == 1)
-                {
-                    if (element.FirstChild is XmlText)
-                    {
-                        ExecuteCore(element.FirstChild as XmlText);
-                    }
-                    else if (element.FirstChild is XmlCDataSection)
-                    {
-                        ExecuteCore(element.FirstChild as XmlCDataSection);
-                    }
-                }
-            }
-            else
-            {
-                var newElement = element.OwnerDocument.CreateTextNode(Value);
-                element.AppendChild(newElement);
-            }
-        }
-
-        protected override void ExecuteCore(XmlAttribute attribute)
-        {
-            attribute.Value = Value + attribute.Value;
-        }
-
-        protected override void ExecuteCore(XmlText text)
-        {
-            text.Value = Value + text.Value;
-        }
-
-        protected override void ExecuteCore(XmlCDataSection section)
-        {
-            section.Value = Value + section.Value;
-        }
-
-        protected override void ExecuteCore(XmlComment comment)
-        {
-            comment.Value = Value + comment.Value;
-        }
-
-        protected override void ExecuteCore(XmlProcessingInstruction instruction)
-        {
-            instruction.Value = Value + instruction.Value;
+            return Value + value;
         }
     }
 }
