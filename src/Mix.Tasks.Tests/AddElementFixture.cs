5e5f068539e1e7b5197e15028d119630ef9a43f7
Richard Hubers <richard.hubers@gmail.com>
2009-11-01 22:44:00 +0100

    Added tests for add-element.

--- src/Mix.Tasks.Tests/AddElementFixture.cs
+++ src/Mix.Tasks.Tests/AddElementFixture.cs
@@ -1,3 +1,4 @@
+using Mix.Core.Exceptions;
 using NUnit.Framework;
 
 namespace Mix.Tasks.Tests
@@ -8,51 +9,89 @@ namespace Mix.Tasks.Tests
         [Test]
         public void AddElementWithoutValue()
         {
-            const string pre = @"<root />";
-            const string post = @"<root><name></name></root>";
-            const string xpath = "root";
+            const string Pre = @"<root />";
+            const string Post = @"<root><name></name></root>";
+            const string XPath = "root";
             var task = new AddElement {Name = "name"};
-            Run(pre, post, xpath, task);
+            Run(Pre, Post, XPath, task);
         }
 
         [Test]
         public void AddElementWithValue()
         {
-            const string pre = @"<root />";
-            const string post = @"<root><name>value</name></root>";
-            const string xpath = "root";
+            const string Pre = @"<root />";
+            const string Post = @"<root><name>value</name></root>";
+            const string XPath = "root";
             var task = new AddElement {Name = "name", Value = "value"};
-            Run(pre, post, xpath, task);
+            Run(Pre, Post, XPath, task);
         }
 
         [Test]
         public void AddElementBefore()
         {
-            const string pre = @"<root><one /><two /><three /></root>";
-            const string post = @"<root><one /><name>value</name><two /><three /></root>";
-            const string xpath = "root";
+            const string Pre = @"<root><one /><two /><three /></root>";
+            const string Post = @"<root><one /><name>value</name><two /><three /></root>";
+            const string XPath = "root";
             var task = new AddElement {Name = "name", Value = "value", Before = "two"};
-            Run(pre, post, xpath, task);
+            Run(Pre, Post, XPath, task);
+        }
+
+        [Test]
+        public void AddElementBeforeWithNonSelectingXPath()
+        {
+            const string Pre = @"<root><one /><two /><three /></root>";
+            const string Post = @"<root><one /><two /><three /><name>value</name></root>";
+            const string XPath = "root";
+            var task = new AddElement {Name = "name", Value = "value", Before = "non-existing"};
+            Run(Pre, Post, XPath, task);
         }
 
         [Test]
         public void IfNoElementSelectedJustAppend()
         {
-            const string pre = @"<root><list><one /><two /><three /></list><list><one /><three /></list></root>";
-            const string post = @"<root><list><one /><name>value</name><two /><three /></list><list><one /><three /><name>value</name></list></root>";
-            const string xpath = "root/list";
+            const string Pre = @"<root><list><one /><two /><three /></list><list><one /><three /></list></root>";
+            const string Post = @"<root><list><one /><name>value</name><two /><three /></list><list><one /><three /><name>value</name></list></root>";
+            const string XPath = "root/list";
             var task = new AddElement {Name = "name", Value = "value", Before = "two"};
-            Run(pre, post, xpath, task);
+            Run(Pre, Post, XPath, task);
+        }
+
+        [Test, ExpectedException(typeof(TaskExecutionException))]
+        public void AddElementBeforeWithInvalidXPath()
+        {
+            const string Pre = @"<root />";
+            const string XPath = "root";
+            var task = new AddElement {Name = "name", Value = "value", Before = "///"};
+            Run(Pre, null, XPath, task);
         }
 
         [Test]
         public void AddElementAfter()
         {
-            const string pre = @"<root><one /><two /><three /></root>";
-            const string post = @"<root><one /><two /><name>value</name><three /></root>";
-            const string xpath = "root";
+            const string Pre = @"<root><one /><two /><three /></root>";
+            const string Post = @"<root><one /><two /><name>value</name><three /></root>";
+            const string XPath = "root";
             var task = new AddElement {Name = "name", Value = "value", After = "two"};
-            Run(pre, post, xpath, task);
+            Run(Pre, Post, XPath, task);
+        }
+
+        [Test]
+        public void AddElementAfterWithNonSelectingXPath()
+        {
+            const string Pre = @"<root><one /><two /><three /></root>";
+            const string Post = @"<root><one /><two /><three /><name>value</name></root>";
+            const string XPath = "root";
+            var task = new AddElement {Name = "name", Value = "value", After = "non-existing"};
+            Run(Pre, Post, XPath, task);
+        }
+
+        [Test, ExpectedException(typeof(TaskExecutionException))]
+        public void AddElementAfterWithInvalidXPath()
+        {
+            const string Pre = @"<root />";
+            const string XPath = "root";
+            var task = new AddElement {Name = "name", Value = "value", After = "///"};
+            Run(Pre, null, XPath, task);
         }
     }
 }
