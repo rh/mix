f9769f9cee7ed1728c4a3377e3d8ffe608d29a93
Richard Hubers <richard.hubers@gmail.com>
2009-11-03 21:29:13 +0100

    Added before and after options to add-fragment.
    These options can be used to specify before or after which node the
    fragment will be added.

--- src/Mix.Tasks.Tests/AddFragmentFixture.cs
+++ src/Mix.Tasks.Tests/AddFragmentFixture.cs
@@ -1,3 +1,4 @@
+using Mix.Core.Exceptions;
 using NUnit.Framework;
 
 namespace Mix.Tasks.Tests
@@ -5,15 +6,75 @@ namespace Mix.Tasks.Tests
     [TestFixture]
     public class AddFragmentFixture : TestFixture
     {
+        private const string Fragment = "<child>Some text</child>";
+        private const string InvalidXPath = "///";
+
         [Test]
-        public void Element()
+        public void AddFragmentAtDefaultPosition()
         {
-            const string fragment = "<child>Some text</child>";
-            const string pre = "<root />";
-            const string post = "<root>" + fragment + "</root>";
-            const string xpath = "root";
-            var task = new AddFragment {Fragment = fragment};
-            Run(pre, post, xpath, task);
+            const string Pre = "<root />";
+            const string Post = "<root>" + Fragment + "</root>";
+            const string XPath = "root";
+            var task = new AddFragment {Fragment = Fragment};
+            Run(Pre, Post, XPath, task);
+        }
+
+        [Test]
+        public void AddFragmentBefore()
+        {
+            const string Pre = "<root><first /><last /></root>";
+            const string Post = "<root>" + Fragment + "<first /><last /></root>";
+            const string XPath = "root";
+            var task = new AddFragment {Fragment = Fragment, Before = "first"};
+            Run(Pre, Post, XPath, task);
+        }
+
+        [Test]
+        public void InsertBeforeWithNonSelectingXPath()
+        {
+            const string Pre = "<root><first /><last /></root>";
+            const string Post = "<root><first /><last />" + Fragment + "</root>";
+            const string XPath = "root";
+            var task = new AddFragment {Fragment = Fragment, Before = "non-existing"};
+            Run(Pre, Post, XPath, task);
+        }
+
+        [Test, ExpectedException(typeof(TaskExecutionException))]
+        public void InsertBeforeWithInvalidXPath()
+        {
+            const string Pre = "<root><first /><last /></root>";
+            const string XPath = "root";
+            var task = new AddFragment {Fragment = Fragment, Before = InvalidXPath};
+            Run(Pre, null, XPath, task);
+        }
+
+        [Test]
+        public void AddFragmentAfter()
+        {
+            const string Pre = "<root><first /><last /></root>";
+            const string Post = "<root><first />" + Fragment + "<last /></root>";
+            const string XPath = "root";
+            var task = new AddFragment {Fragment = Fragment, After = "first"};
+            Run(Pre, Post, XPath, task);
+        }
+
+        [Test]
+        public void InsertAfterWithNonSelectingXPath()
+        {
+            const string Pre = "<root><first /><last /></root>";
+            const string Post = "<root><first /><last />" + Fragment + "</root>";
+            const string XPath = "root";
+            var task = new AddFragment {Fragment = Fragment, After = "non-existing"};
+            Run(Pre, Post, XPath, task);
+        }
+
+        [Test, ExpectedException(typeof(TaskExecutionException))]
+        public void InsertAfterWithInvalidXPath()
+        {
+            const string Pre = "<root><first /><last /></root>";
+            const string XPath = "root";
+            var task = new AddFragment {Fragment = Fragment, After = InvalidXPath};
+            Run(Pre, null, XPath, task);
         }
     }
 }
--- src/Mix.Tasks/AddFragment.cs
+++ src/Mix.Tasks/AddFragment.cs
@@ -1,6 +1,8 @@
 using System.Xml;
+using System.Xml.XPath;
 using Mix.Core;
 using Mix.Core.Attributes;
+using Mix.Core.Exceptions;
 
 namespace Mix.Tasks
 {
@@ -11,10 +13,55 @@ namespace Mix.Tasks
         [Description("The raw XML of the new element.")]
         public string Fragment { get; set; }
 
+        [Option]
+        [Description("An XPath expression, applied to the selected element, which determines before which child the new element is added.")]
+        public string Before { get; set; }
+
+        [Option]
+        [Description("An XPath expression, applied to the selected element, which determines after which child the new element is added.")]
+        public string After { get; set; }
+
         protected override void ExecuteCore(XmlElement element)
         {
             var child = element.OwnerDocument.CreateDocumentFragment();
             child.InnerXml = Fragment;
+
+            if (!string.IsNullOrEmpty(After))
+            {
+                try
+                {
+                    var node = element.SelectSingleNode(After);
+                    if (node != null)
+                    {
+                        element.InsertAfter(child, node);
+                        return;
+                    }
+                }
+                catch (XPathException e)
+                {
+                    var message = string.Format("'{0}' is not a valid XPath expression.", After);
+                    throw new TaskExecutionException(message, e);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(Before))
+            {
+                try
+                {
+                    var node = element.SelectSingleNode(Before);
+                    if (node != null)
+                    {
+                        element.InsertBefore(child, node);
+                        return;
+                    }
+                }
+                catch (XPathException e)
+                {
+                    var message = string.Format("'{0}' is not a valid XPath expression.", Before);
+                    throw new TaskExecutionException(message, e);
+                }
+            }
+
             element.AppendChild(child);
         }
     }
