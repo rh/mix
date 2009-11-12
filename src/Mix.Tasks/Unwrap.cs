d4fe736a40ab9218ec44a74cdebb062b93061f82
Richard Hubers <richard.hubers@gmail.com>
2009-11-12 09:18:14 +0100

    Unwrap only deals with elements.

--- src/Mix.Tasks/Unwrap.cs
+++ src/Mix.Tasks/Unwrap.cs
@@ -4,7 +4,7 @@ using Mix.Core.Attributes;
 
 namespace Mix.Tasks
 {
-	[Description("Unwraps the selected nodes, e.g. '<element>text</element>' becomes 'text' and '<!-- text -->' becomes ' text '.")]
+	[Description("Unwraps the selected elements, e.g. '<element>text</element>' becomes 'text'.")]
 	public class Unwrap : Task
 	{
 		protected override void ExecuteCore(XmlElement element)
@@ -15,27 +15,6 @@ namespace Mix.Tasks
 				element.ParentNode.InsertBefore(clone, element);
 			}
 			element.ParentNode.RemoveChild(element);
-		}
-
-		protected override void ExecuteCore(XmlCDataSection section)
-		{
-			var clone = section.OwnerDocument.CreateTextNode(section.Value);
-			section.ParentNode.InsertBefore(clone, section);
-			section.ParentNode.RemoveChild(section);
-		}
-
-		protected override void ExecuteCore(XmlComment comment)
-		{
-			var clone = comment.OwnerDocument.CreateTextNode(comment.Value);
-			comment.ParentNode.InsertBefore(clone, comment);
-			comment.ParentNode.RemoveChild(comment);
-		}
-
-		protected override void ExecuteCore(XmlProcessingInstruction instruction)
-		{
-			var clone = instruction.OwnerDocument.CreateTextNode(instruction.Value);
-			instruction.ParentNode.InsertBefore(clone, instruction);
-			instruction.ParentNode.RemoveChild(instruction);
 		}
 	}
 }
