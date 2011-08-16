3a04138e471ae59f616b8068559c32f1263151c7
Richard Hubers <richard.hubers@gmail.com>
2011-08-16 21:43:55 +0200

    Extracted TaskValidator from Task

---
+++ src/Mix/TaskValidator.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Xml;
+using Mix.Attributes;
+using Mix.Exceptions;
+using Mix.Extensions;
+
+namespace Mix
+{
+    public static class TaskValidator
+    {
+        public static void Validate(Task task)
+        {
+            foreach (var property in task.GetType().GetProperties())
+            {
+                if (property.IsAnOption() && property.IsRequired())
+                {
+                    var value = property.GetValue(task, null);
+                    if (value == null || value.ToString().Length == 0)
+                    {
+                        var message = String.Format("'{0}' is required.", property.Name.ToLower());
+                        var description = "";
+                        if (property.IsDefined(typeof(DescriptionAttribute), false))
+                        {
+                            var attribute = (DescriptionAttribute) property.GetCustomAttributes(typeof(DescriptionAttribute), true)[0];
+                            description = attribute.Description;
+                        }
+                        throw new RequirementException(message, property.Name, description);
+                    }
+                }
+
+                if (XmlOptionAttribute.IsDefinedOn(property))
+                {
+                    var value = property.GetValue(task, null);
+                    if (value != null && value.ToString().Trim().Length > 0)
+                    {
+                        var xml = value as string;
+                        if (xml != null)
+                        {
+                            try
+                            {
+                                var document = new XmlDocument();
+                                // Create a root node, because the XML is just a fragment.
+                                document.LoadXml(String.Format("<root>{0}</root>", xml));
+                            }
+                            catch (XmlException e)
+                            {
+                                var message = String.Format("Option '{0}' ('{1}') is not valid XML: {2}", property.Name.ToLower(), xml, e.Message);
+                                throw new XmlException(message, e);
+                            }
+                        }
+                    }
+                }
+
+                if (RegexOptionAttribute.IsDefinedOn(property))
+                {
+                    var value = property.GetValue(task, null);
+                    if (value != null && value.ToString().Trim().Length > 0)
+                    {
+                        var pattern = value as string;
+                        if (pattern != null)
+                        {
+                            try
+                            {
+                                new Regex(pattern);
+                            }
+                            catch (ArgumentException e)
+                            {
+                                var message = String.Format("Option '{0}' is not a valid regular expression: {1}", property.Name.ToLower(), e.Message);
+                                throw new ArgumentException(message, e);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
